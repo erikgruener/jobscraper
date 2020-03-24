d0b0a3c06c009269576904ee385a835a4e427cf7
erikgruener18 <gruener@campus.tu-berlin.de>
2020-03-24 16:52:41 +0100

    added page numbers, because it looked like a lot of duplicates, and added an example csv to see how it looks

--- indeed_scrape.py
+++ indeed_scrape.py
@@ -2,7 +2,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-import sys
+#import sys
 
 def get_page_count(job,loc):
     source_text = requests.get('https://de.indeed.com/jobs?q='+job+'&l='+loc).text
@@ -17,8 +17,7 @@ def progress(count, total, status=''):
     filled_len = int(round(bar_len * count / float(total)))
     percents = round(100.0 * count / float(total), 1)
     bar = '=' * filled_len + '-' * (bar_len - filled_len)
-    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
-    sys.stdout.flush()
+    print("\r {} {} {}".format(bar, percents, status),end=" ")
 
 def create_job_list(page_count,job,loc,nogo_list,csv_writer):
     for page in range(page_count):
@@ -28,27 +27,26 @@ def create_job_list(page_count,job,loc,nogo_list,csv_writer):
 
         #Because sections can be missing like salary use try/except
         for y in soup.find_all('div',class_='jobsearch-SerpJobCard unifiedRow row result'):
-            job_title_works = y.find('div', class_='title').a.text
-            company = y.find('span',class_='company').text
+            job_title = y.find('div', class_='title').a.text.strip("\n")
+            company = y.find('span',class_='company').text.strip("\n")
             if company in nogo_list:
                 continue
             link = 'https://de.indeed.com'+y.find('div', class_='title').a.get('href')
             try:
-                salary = y.find('span',class_='salaryText').text
+                salary = y.find('span',class_='salaryText').text.strip("\n")
             except:
                 salary=None
-            csv_writer.writerow([job_title_works,company,salary,link])
-
+            csv_writer.writerow([page,job_title,company,salary,link])
+            
 if __name__ == "__main__":
 
-    job = 'Job'
-    loc = 'City'
-    nogo_list = ['Firm1','Firm2']
-    page_count = get_page_count(job,loc)
+    job = 'Werkstudent'
+    loc = 'Berlin'
+    nogo_list = []
+    page_count = get_page_count(job,loc)#get_page_count(job,loc)
 
-    csv_file = open('indeed_scrape.csv','w')
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Job_Title','Company','Salary','Link'])
-
+    csv_file = open("example.csv", "w",newline='')
+    csv_writer = csv.writer(csv_file,delimiter=',')
+    csv_writer.writerow(['Page','Job_Title','Company','Salary','Link'])
     create_job_list(page_count,job,loc,nogo_list,csv_writer)
     csv_file.close()
